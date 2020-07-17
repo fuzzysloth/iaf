71cdf8fb80b2d76657e1508c1d9a46317c8f059c
Stanislav Tsekhmistro <stas-t@gmx.com>
2020-07-17 13:49:19 +0200

    Create and wire up JdbcFacade instance.

--- test/src/test/java/nl/nn/adapterframework/integration/MSSQLIntegrationTest.java
+++ test/src/test/java/nl/nn/adapterframework/integration/MSSQLIntegrationTest.java
@@ -3,10 +3,12 @@ package nl.nn.adapterframework.integration;
 import org.apache.logging.log4j.Logger;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
+import nl.nn.adapterframework.jdbc.JdbcFacade;
 import nl.nn.adapterframework.util.LogUtil;
 
 @RunWith(SpringJUnit4ClassRunner.class)
@@ -15,8 +17,11 @@ import nl.nn.adapterframework.util.LogUtil;
 public class MSSQLIntegrationTest {
 	private static final Logger LOG = LogUtil.getLogger(MSSQLIntegrationTest.class);
 
+	@Autowired
+	private JdbcFacade jdbcFacade;
+	
 	@Test
 	public void testOne() throws Exception {
-		LOG.info("Hallo!");
+		LOG.info("jdbcFacade=", jdbcFacade);
 	}
 }
